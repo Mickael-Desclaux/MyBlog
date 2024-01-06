f67741047ec95a6ed8fa545713a86df8a18ac4c7
Mickael <mickaeldesclaux@gmail.com>
2024-01-06 15:42:24 +0100

    Add Article Controller and Model

---
+++ MyBlog.Api/Controllers/ArticleController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.Api.Models;
+
+namespace MyBlog.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ArticleController
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Getting all the articles";
+    }
+
+    [HttpGet("{id:int}")]
+    public string GetById(int id)
+    {
+        return $"Getting article with ID : {id}";
+    }
+
+    [HttpPost]
+    public string Create([FromForm]Article article)
+    {
+        return "Creating a article";
+    }
+
+    [HttpPut("{id:int}")]
+    public string Update(int id)
+    {
+        return $"Updating article with id : {id}";
+    }
+
+    [HttpDelete("{id:int}")]
+    public string Delete(int id)
+    {
+        return $"Deleting article with id: {id}";
+    }
+}
---
+++ MyBlog.Api/Models/Article.cs
@@ -0,0 +1,22 @@
+﻿namespace MyBlog.Api.Models;
+
+public class Article
+{
+    public int ArticleId { get; set; }
+    
+    public int TagID { get; set; }
+    
+    public string? BookCover { get; set; }
+    
+    public string? TextSection { get; set; }
+    
+    public string? ReviewResume { get; set; }
+    
+    public float? MyNote { get; set; }
+    
+    public List<string?>? Quotes { get; set; }
+    
+    public DateTime? CreatedAt { get; set; }
+    
+    public DateTime? UpdatedAt { get; set; }
+}
